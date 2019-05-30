7b4fcf3e122689b35eba0e6ff3891b42f9e82d6d
Antavio <njuguna13@gmail.com>
2019-05-30 23:43:38 +0300

    Create a Profile test instance and save methods.

--- hood_app/tests.py
+++ hood_app/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import *
 
-# Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='password')
+        self.new_profile = Profile(id=1,prof_user=self.new_user,user_location='Turkana')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,Profile))
+
+    def test_save_profile(self):
+        self.new_profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
