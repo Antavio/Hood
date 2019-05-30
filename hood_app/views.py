1c2d38892fc8f9f2bca898b843552de9ebad95f3
Antavio <njuguna13@gmail.com>
2019-05-30 08:40:23 +0300

    Add a new_profile view function.

--- hood_app/views.py
+++ hood_app/views.py
@@ -5,3 +5,18 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='/accounts/login/')
 def home(request):
     return render(request,'hood_app/index.html')
+
+@login_required(login_url='/accounts/login/')
+def new_profile(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES)
+        if form.is_valid():
+            profile = form.save(commit=False)
+            profile.prof_user = current_user
+            profile.profile_Id = request.user.id
+            profile.save()
+        return redirect('profile')
+    else:
+        form = ProfileForm()
+    return render(request, 'profile/new_profile.html', {"form": form})
