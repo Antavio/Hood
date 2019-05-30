864a41fdb7e3298ec5583ca492ff65db3d083a3f
Antavio <njuguna13@gmail.com>
2019-05-30 11:55:38 +0300

    Register my models to the admin panel.

--- hood_app/models.py
+++ hood_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+class Neighborhood(models.Model):
+    hood_name = models.CharField(max_length=100)
+    hood_location = models.CharField(max_length=100)
+    occupants = models.IntegerField(default=0)
+
+
+class Profile(models.Model):
+    profile_picture = models.ImageField(upload_to='prof_pics/',blank=True)
+    user_location = models.CharField(max_length=200)
+    prof_user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    hood_id = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
+
+class Business(models.Model):
+    business_name = models.CharField(max_length=200)
+    business_email = models.EmailField()
+    business_owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    business_hood_id = models.ForeignKey(Neighborhood,on_delete=models.CASCADE,null=True)
