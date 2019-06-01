fd62b33e8f3aef1c2f952d22189b4dc6c95f8548
Antavio <njuguna13@gmail.com>
2019-06-01 13:28:11 +0300

    Add post test_instance,save_instance and delete_instance.

--- hood_app/tests.py
+++ hood_app/tests.py
@@ -78,3 +78,43 @@ class BusinessTestClass(TestCase):
         bizz = Business.search_business(1)
         bizz.business_name = 'Updated Bizna'
         self.assertEqual(bizz.business_name,'Updated Bizna')
+
+
+class PostTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='password')
+        self.new_hood = Neighborhood(id=1,hood_name='Mtaani',hood_location="Nairobi",occupants=10)
+        self.new_hood.save_hood()
+        self.new_post = Post(id=1,post_name="Posting",post_description='Post Description',date_posted='2018-05-04',post_owner=self.new_user,hood_post=self.new_hood)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_post,Post))
+
+    def test_save_instance(self):
+        self.new_post.save_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post)>0)
+
+    def test_delete_instance(self):
+        self.new_post.delete_post()
+        post = Post.objects.all()
+        self.assertTrue(len(post)==0)
+
+class ContactTestClass(TestCase):
+    def setUp(self):
+        self.new_hood = Neighborhood(id=1,hood_name='Mtaani',hood_location="Nairobi",occupants=10)
+        self.new_hood.save_hood()
+        self.new_contact = ContactInfo(id=1,health_department='Health',police_department='Police',hood=self.new_hood)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_contact,ContactInfo))
+
+    def test_save_instance(self):
+        self.new_contact.save_contact()
+        contact = ContactInfo.objects.all()
+        self.assertTrue(len(contact)>0)
+
+    def test_delete_instance(self):
+        self.new_contact.delete_contact()
+        contact = ContactInfo.objects.all()
+        self.assertTrue(len(contact)==0)
